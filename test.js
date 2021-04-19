3caff4b4ab1f97278f332bbbfb0f3f9723b01ffd
David Guttman <david@davidguttman.com>
2021-04-18 22:21:01 -0700

    fiat lux

--- test.js
+++ test.js
@@ -7,24 +7,45 @@ const server = require('./server')
 const urlBase = `http://localhost:${PORT}`
 
 tape('should respond hello', (t) => {
-  jsonist.get(urlBase, (err, body) => {
-    if (err) t.error(err)
+    jsonist.get(urlBase, (err, body) => {
+        if (err) t.error(err)
 
-    t.equal(body.msg, 'hello')
-    t.end()
-  })
+        t.equal(body.msg, 'hello')
+        t.end()
+    })
 })
 
 tape('should respond b64', (t) => {
-  jsonist.get(`${urlBase}/b64/hello`, (err, body) => {
-    if (err) t.error(err)
+    jsonist.get(`${urlBase}/b64/hello`, (err, body) => {
+        if (err) t.error(err)
 
-    t.equal(body.b64, 'aGVsbG8=')
-    t.end()
-  })
+        t.equal(body.b64, 'aGVsbG8=')
+        t.end()
+    })
 })
 
-tape('cleanup', function (t) {
-  server.close()
-  t.end()
+tape('cleanup', function(t) {
+    server.close()
+    t.end()
+})
+const tape = require('tape')
+const jsonist = require('jsonist')
+
+const PORT = process.env.PORT = process.env.PORT || require('get-PORT-sync')()
+const server = require('./server')
+
+const urlBase = `http://localhost:${PORT}`
+
+tape('should respond hello', (t) => {
+    jsonist.get(urlBase, (err, body) => {
+        if (err) t.error(err)
+
+        t.equal(body.msg, 'hello')
+        t.end()
+    })
+})
+
+tape('cleanup', function(t) {
+    server.close()
+    t.end()
 })
